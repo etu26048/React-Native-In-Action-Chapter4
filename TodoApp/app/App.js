1e833e9e16e180593b7378e7b92143e79f8cbec9
Lazrak Yassin <yassin@MBP-de-Lazrak.neofactomikro>
2019-08-27 09:34:09 +0200

    Firt commit at Chapter 4

---
+++ TodoApp/app/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <View style={{ marginLeft: 20, marginTop: 20 }}>
+        <Text style={{ fontSize: 18, color: 'red' }}>Some Text</Text>
+      </View>
+    )
+  }
+}
+
